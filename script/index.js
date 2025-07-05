c8eb76316e8e4e5001c6112b4eb316b124edf334
JI0 <alsn8539@gmail.com>
2025-07-05 23:28:40 +0900

    업로드 오류

--- script/index.js
+++ script/index.js
@@ -1,239 +1,141 @@
-//메인배너
-const ani = document.querySelectorAll('.bnr_txt');
-console.log(ani);
+// 스와이퍼 슬라이드
+const main = new Swiper('#main',{
+    mousewheel:true,
+    direction:'vertical',
+    behavior:'smooth',
+    speed: 1000,
+});
 
-const mainBnr = new Swiper('.main_bnr',{
-    autoplay:{
-        delay:5000,
-    },
+const bnrSlide = new Swiper('.design .left .banner',{
+    autoplay:{delay:0},
     loop:true,
-    on: {
-        slideChange: function () {
-            for(let i of ani) i.classList.remove('js_bnr')
-            ani[this.realIndex].classList.add('js_bnr')
-        }
-    },
-    scrollbar: {
-        el: ".main_bnr .swiper-scrollbar",
+    speed:3500,
+    breakpoints:{
+        1400:{slidesPerView:3, direction: 'horizontal'}, /* => 화면 너비 1024 이상 시 2개 표시 (1400~max) */
+        1024:{slidesPerView:1, direction: 'vertical'}, /* => 화면 너비 1024 이상 시 2개 표시 (1024~1400) */
+        480:{slidesPerView:1, direction: 'vertical'}, /* => 화면 너비 480 이상 시 2개 표시 (480~1024) */
     },
 });
 
-//신상
-const newSwiper = new Swiper('.new',{
-    autoplay:{
-        delay:5000,
-    },
+const snsSlide = new Swiper('.design .left .sns',{
+    autoplay:{delay:0},
     loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".new_wrap .swiper-scrollbar",
+    speed:4500,
+    slidesPerView:3,
+    breakpoints:{
+        1400:{slidesPerView:3,},/* => 화면 너비 1024 이상 시 2개 표시 (1400~max) */
+        1024:{slidesPerView:2,},/* => 화면 너비 1024 이상 시 2개 표시 (1024~1400) */
+        500:{slidesPerView:2,}, /* => 화면 너비 480 이상 시 2개 표시 (1024~500) */
+        430:{slidesPerView:1,}, /* => 화면 너비 480 이상 시 2개 표시 (1024~500) */
     },
 });
 
-// ============================================= 베스트
-//전체보기
-const bestSwiper = new Swiper('.best1',{
-    autoplay:{
-        delay:500000,
-    },
+const detailSlide = new Swiper('.design .right .detail',{
+    autoplay:{delay:2500,},
+    // effect:'fade',
     loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".alll .progress .swiper-scrollbar",
+    breakpoints:{
+        1400:{slidesPerView:1,},/* => 화면 너비 1024 이상 시 1개 표시 (1400~max) */
+        480:{slidesPerView:3,}, /* => 화면 너비 480 이상 시 3개 표시 (500~1400) */
+        429:{slidesPerView:1,}, /* => 화면 너비 480 이상 시 1개 표시 (1024~500) */
     },
-});
-//라면
-const noodleSwiper = new Swiper('.best2',{
-    autoplay:{
-        delay:500000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".noodle .progress .swiper-scrollbar",
-    },
-});
-//오뚜기밥
-const riceSwiper = new Swiper('.best3',{
-    autoplay:{
-        delay:500000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".rice .progress .swiper-scrollbar",
-    },
-});
-//스낵
-const crackerSwiper = new Swiper('.best4',{
-    autoplay:{
-        delay:500000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".cracker .progress .swiper-scrollbar",
-    },
-});
-//3분
-const threeSwiper = new Swiper('.best5',{
-    autoplay:{
-        delay:500000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".three .progress .swiper-scrollbar",
-    },
-});
-//간편
-const easySwiper = new Swiper('.best6',{
-    autoplay:{
-        delay:500000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".easy .progress .swiper-scrollbar",
-    },
-});
-
-// 베스트리뷰
-const reviewSwiper = new Swiper('.review',{
-    autoplay:{
-        delay:5000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".review_wrap .swiper-scrollbar",
-    },
-});
-//베스트탭내용
-const bestTab = document.querySelectorAll('.best_wrap .best_tab > div');
-const bestTitle = document.querySelectorAll('.best_wrap .item_tab a');
-console.log(bestTab,bestTitle);
-function bestHide(){
-    for(let i of bestTab) i.style.display = 'none';
-};
-
-function bestClsReset(){
-    for(let i of bestTitle) i.classList.remove('active');
-};
-bestHide();
-bestClsReset();
-
-bestTab[0].style.display = 'block';
-bestTitle[0].classList.add('active');
-
-bestTitle.forEach((target,index)=>{
-    target.addEventListener('click',(e)=>{
-        bestClsReset();
-        target.classList.add('active');
-
-        bestHide();
-        bestTab[index].style.display = 'block';
-
-        e.preventDefault();
-    })
+    pagination:{
+        el:'.design .swiper-pagination',
+        type:'bullets'
+    }
 })
 
 
-//====================== 오직 오뚜기몰 탭
-//전체보기
-const onlySwiper = new Swiper('.only1',{
-    autoplay:{
-        delay:5000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".all .progress .swiper-scrollbar",
-    },
-});
-//단독
-const singleSwiper = new Swiper('.only2',{
-    autoplay:{
-        delay:5000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".single .progress .swiper-scrollbar",
-    },
-});
-//스낵
-const snackSwiper = new Swiper('.only3',{
-    autoplay:{
-        delay:5000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".snack .progress .swiper-scrollbar",
-    },
-});
-//간편식
-const simpleSwiper = new Swiper('.only4',{
-    autoplay:{
-        delay:5000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".simple .progress .swiper-scrollbar",
-    },
-});
-//라면
-const ramenSwiper = new Swiper('.only5',{
-    autoplay:{
-        delay:5000,
-    },
-    loop:true,
-    slidesPerView:4,
-    spaceBetween:13,
-    scrollbar: {
-        el: ".ramen .progress .swiper-scrollbar",
-    },
-});
+// =========================== nav 클릭이벤트
+const nav = document.querySelectorAll('nav a');
+const contact = document.querySelector('.h_right')
+const design = document.querySelector('nav li:nth-child(4) a')
+console.log(design)
 
-//오뚜기몰탭내용
-const onlyTab = document.querySelectorAll('.only_wrap .only_tab > div');
-const onlyTitle = document.querySelectorAll('.only_wrap .item_tab a');
-
-function onlyHide(){/* 탭 내용 모두 숨기기 */
-    for(let i of onlyTab) i.style.display = 'none';
+function navRest(){
+    for(let i of nav) i.classList.remove('nav_active');
 };
+navRest();
+nav[0].classList.add('nav_active');
 
-function onlyClsReset(){ /* 탭 클래스 초기화 */
-    for(let i of onlyTitle) i.classList.remove('active');
-}
-onlyHide();
-onlyClsReset();
-/* 초기 화면 보이기 */
-onlyTab[0].style.display = 'block';
-onlyTitle[0].classList.add('active');
-
-onlyTitle.forEach((target,index)=>{
-    target.addEventListener('click',(e)=>{
-        onlyClsReset();
-        target.classList.add('active');
-
-        onlyHide();
-        onlyTab[index].style.display = 'block';
-
+nav.forEach((t, i)=>{ 
+    t.addEventListener('click',function(e){
         e.preventDefault();
+        navRest();
+        t.classList.add('nav_active');
+        main.slideTo(i, 1000);
+        setTimeout(() => {
+            ScrollTrigger.refresh();
+        }, 0);
     })
+})
+
+design.addEventListener('click',function(e){ //3행 소개영역으로 이동
+    e.preventDefault();
+    main.slideTo(6, 1000);
+});
+
+contact.addEventListener('click',function(e){ //6행 소개영역으로 이동
+    e.preventDefault();
+    main.slideTo(7, 1000);
+});
+
+//팝업 띄우기
+const popup_bg = document.querySelector('.popup_bg');
+const bnr = document.querySelectorAll('.banner img');
+const sns = document.querySelectorAll('.sns img');
+const detail = document.querySelectorAll('.detail img');
+const scroll = document.querySelector('.scroll_wrap');
+/* console.log(popup_bg, bnr); */
+
+popup_bg.style.display = 'none' // 팝업 숨기기
+
+//================배너 팝업
+for(let i of bnr){
+    i.addEventListener('click',()=>{
+        popup_bg.style.display = 'block'
+        console.log(i);
+        
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '900px';
+        popup_bg.children[0].style.marginTop = '200px';
+
+        main.mousewheel.disable();
+    })
+}
+
+//================SNS 팝업
+for(let i of sns){
+    i.addEventListener('click',()=>{
+        popup_bg.style.display = 'block'
+        console.log(i);
+        
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '700px';
+        popup_bg.children[0].style.marginTop = '150px';
+
+        main.mousewheel.disable();
+    })
+}
+
+//================상세페이지 팝업
+for(let i of detail){
+    i.addEventListener('click',()=>{
+        popup_bg.style.display = 'block'
+        console.log(i);
+        
+        popup_bg.children[0].children[0].src = i.src;
+        popup_bg.children[0].style.width = '700px';
+        popup_bg.children[0].style.marginTop = '90px';
+
+        scroll.scrollTo(0,0);
+
+        main.mousewheel.disable();
+    })
+}
+
+//팝업 닫기
+popup_bg.addEventListener('click',()=>{
+    popup_bg.style.display = 'none';
+    main.mousewheel.enable(); //마우스휠 허용(팝업 숨길 시)
 })
